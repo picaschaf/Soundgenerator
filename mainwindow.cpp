739536e34453d630eef0a354d2828c33ad7cd83d
FeZar97 <bhd2010@yandex.ru>
2020-10-29 23:22:45 +0300

    code modified for Qt5 version

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -5,34 +5,21 @@
 
 #include "qxtsoundgenerator.h"
 
-
-MainWindow::MainWindow(QWidget *parent) :
-    QMainWindow(parent),
-    ui(new Ui::MainWindow)
-{
+MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow) {
     ui->setupUi(this);
-
-
 
     QxtSoundGenerator* generator = new QxtSoundGenerator();
 
-
     // Komisches Tetris
 
-    const int durationLong  = 600;
     const int durationShort = 400;
-    const qreal amplitude   = 0.05;
+    const qreal amplitude   = 16383;
     const int c = 523;
     const int d = 587;
     const int e = 659;
     const int f = 698;
     const int g = 784;
     const int a = 880;
-    const int h = 987;
-    const int c_ = 554;
-    const int d_ = 622;
-    const int f_ = 740;
-    const int g_ = 830;
     const int a_ = 932;
 
     generator->appendSound(amplitude, a, durationShort);
@@ -77,7 +64,6 @@ MainWindow::MainWindow(QWidget *parent) :
     generator->play();
 }
 
-MainWindow::~MainWindow()
-{
+MainWindow::~MainWindow() {
     delete ui;
 }
